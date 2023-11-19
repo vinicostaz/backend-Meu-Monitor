435d356f00c495224be11aa2501881907108f836
Vinicius Costa <vinicius.fonseca132@gmail.com>
2023-11-19 12:44:25 -0300

    put method added

--- demo/src/main/java/com/example/demo/FoodController.java
+++ demo/src/main/java/com/example/demo/FoodController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -33,5 +35,26 @@ public class FoodController {
     @DeleteMapping("/{id}")
     public void deleteFood(@PathVariable Long id) {
         repository.deleteById(id);
-}
+    }
+        
+        @CrossOrigin(origins = "*", allowedHeaders = "*")
+        @PutMapping("/{id}")
+        public ResponseEntity<Void> updateFood(@PathVariable Long id, @RequestBody FoodRequestDTO data) {
+            // Verifique se o ID fornecido existe antes de atualizar
+            if (!repository.existsById(id)) {
+                // Pode lançar uma exceção ou retornar um código de resposta indicando que o recurso não foi encontrado
+                // Aqui, estou usando um exemplo simples retornando um código 404 Not Found
+                throw new ResourceNotFoundException("Food with id " + id + " not found");
+            }
+
+            Food existingFood = repository.getById(id);
+            existingFood.setTitle(data.title());
+            existingFood.setImage(data.image());
+            existingFood.setPrice(data.price());
+
+            repository.save(existingFood);
+
+            return ResponseEntity.noContent().build();
+        }
+        
 }
