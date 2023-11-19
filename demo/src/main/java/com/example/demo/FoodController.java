1820400729ace4bb6d40c4147e01fc4ce39d07ab
Vinicius Costa <vinicius.fonseca132@gmail.com>
2023-11-19 11:28:45 -0300

    adding delete method

--- demo/src/main/java/com/example/demo/FoodController.java
+++ demo/src/main/java/com/example/demo/FoodController.java
@@ -28,4 +28,10 @@ public class FoodController {
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
+    
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public void deleteFood(@PathVariable Long id) {
+        repository.deleteById(id);
+}
 }
--- demo/src/main/java/com/example/demo/FoodRepository.java
+++ demo/src/main/java/com/example/demo/FoodRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+	void deleteById(Long id);
 }
